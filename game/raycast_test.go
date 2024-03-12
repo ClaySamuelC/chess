68d2496d6a0ee408d35818ca58a8b89acd311d7e
ClaySamuelC <claysamuelc@gmail.com>
2024-03-12 01:58:01 -0400

    Add raycast tests and fix raycast function to pass them.

--- game/raycast_test.go
+++ game/raycast_test.go
@@ -22,15 +22,33 @@ func TestRaycast(t *testing.T) {
 	}{
 		{
 			fen:  "8/3p4/8/8/8/8/3P4/8 w HAha - 0 1",
-			name: "should return false",
+			name: "should return false when raycast hits unit of wrong team color first",
 			args: args{11, 0, 1, "White", &map[string]bool{"Rook": true}},
 			want: false,
+		},
+		{
+			fen:  "8/3p4/8/8/8/3B4/3Q4/8 w - - 0 1",
+			name: "should return false when raycast hits unit of wrong rank first",
+			args: args{11, 0, 1, "White", &map[string]bool{"Queen": true, "Rook": true}},
+			want: false,
+		},
+		{
+			fen:  "p7/8/8/8/8/8/8/7B w - - 0 1",
+			name: "should return true when raycast has correct target in path",
+			args: args{0, 1, 1, "White", &map[string]bool{"Queen": true, "Bishop": true}},
+			want: true,
+		},
+		{
+			fen:  "Pp6/8/8/8/8/8/8/8 w - - 0 1",
+			name: "should return true",
+			args: args{0, 1, 0, "Black", &map[string]bool{"Pawn": true}},
+			want: true,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			c, _ := CreateGame(tt.fen)
-			got := c.rayCast(tt.args.pos, tt.args.xDir, tt.args.yDir, tt.args.targetTeam, tt.args.targetRanks)
+			got := c.RayCast(tt.args.pos, tt.args.xDir, tt.args.yDir, tt.args.targetTeam, tt.args.targetRanks)
 			assert.Equal(t, tt.want, got)
 		})
 	}
--- go.mod
+++ go.mod
@@ -2,16 +2,22 @@ module chess
 
 go 1.21.1
 
-require github.com/hajimehoshi/ebiten/v2 v2.6.6
+require (
+	github.com/hajimehoshi/ebiten/v2 v2.6.6
+	github.com/stretchr/testify v1.9.0
+)
 
 require (
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/ebitengine/purego v0.6.0 // indirect
-	github.com/go-gl/glfw/v3.3/glfw v0.0.0-20200707082815-5321531c36a2 // indirect
-	github.com/hajimehoshi/ebiten v1.12.12 // indirect
 	github.com/jezek/xgb v1.1.0 // indirect
+	github.com/niemeyer/pretty v0.0.0-20200227124842-a10e7caefd8e // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
 	golang.org/x/exp/shiny v0.0.0-20230817173708-d852ddb80c63 // indirect
 	golang.org/x/image v0.12.0 // indirect
 	golang.org/x/mobile v0.0.0-20230922142353-e2f452493d57 // indirect
 	golang.org/x/sync v0.3.0 // indirect
 	golang.org/x/sys v0.12.0 // indirect
+	gopkg.in/check.v1 v1.0.0-20200902074654-038fdea0a05b // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
